3daae50e2721d4c18506322cf7b0629c7161e63d
Lruihao <Lruihao@users.noreply.github.com>
2021-12-11 15:10:34 +0000

    :rocket: :lipstick: Style: modify watermark style Lruihao/hugo-blog@5021b2bdcf7304dfc723510ee97ad764394036b5

--- js/_custom.js
+++ js/_custom.js
@@ -72,7 +72,7 @@ const CustomJS = new (function () {
   this.renderWatermark = () => {
     new Watermark({
       content: '@Lruihao',
-      opacity: 0.05
+      opacity: 0.02
     });
     return this;
   };
